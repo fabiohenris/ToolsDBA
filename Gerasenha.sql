f79649dc5a7c44587399bec9d6996073352907f8
fabiohenris <91107101+fabiohenris@users.noreply.github.com>
2025-08-18 16:36:03 -0300

    Create Gerasenha.sql

---
+++ Gerasenha.sql
@@ -0,0 +1,35 @@
+-- Fabio Henrique da Silva
+-- Gerador de senha.
+-- Data 18.08.2025
+-- http://linkedin.com/in/fabio-henriques
+
+DECLARE @Password NVARCHAR(100) = '';
+
+-- Conjuntos de caracteres
+DECLARE @Upper   NVARCHAR(26) = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+DECLARE @Lower   NVARCHAR(26) = 'abcdefghijklmnopqrstuvwxyz';
+DECLARE @Numbers NVARCHAR(10) = '0123456789';
+DECLARE @Special NVARCHAR(20) = '!@#$%^&*()-_=+[]{}|;:,.<>?/';
+DECLARE @Length  NVARCHAR(2) = '32'; -- Quantidade de caractere ,8,16,32,64,128... etc
+
+DECLARE @All NVARCHAR(200) = @Upper + @Lower + @Numbers + @Special;
+
+-- Garante pelo menos 1 de cada tipo
+SET @Password += SUBSTRING(@Upper,   ABS(CHECKSUM(NEWID())) % LEN(@Upper)   + 1, 1);
+SET @Password += SUBSTRING(@Lower,   ABS(CHECKSUM(NEWID())) % LEN(@Lower)   + 1, 1);
+SET @Password += SUBSTRING(@Numbers, ABS(CHECKSUM(NEWID())) % LEN(@Numbers) + 1, 1);
+SET @Password += SUBSTRING(@Special, ABS(CHECKSUM(NEWID())) % LEN(@Special) + 1, 1);
+
+-- Montgem da senha.
+WHILE LEN(@Password) < @Length
+    SET @Password += SUBSTRING(@All, ABS(CHECKSUM(NEWID())) % LEN(@All) + 1, 1);
+
+-- Embaralhar a senha para evitar padrão fixo
+;WITH CTE AS (
+    SELECT SUBSTRING(@Password, v.number+1, 1) AS ch
+    FROM master..spt_values v
+    WHERE v.type='P' AND v.number < LEN(@Password)
+)
+SELECT Password = STRING_AGG(ch, '')
+FROM CTE
+ORDER BY NEWID();
