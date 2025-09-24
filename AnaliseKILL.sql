a015236d2a88ee004f97e4bb94acbde6e12e0640
fabiohenris <91107101+fabiohenris@users.noreply.github.com>
2025-09-24 09:27:15 -0300

    Create AnaliseKILL.sql

---
+++ AnaliseKILL.sql
@@ -0,0 +1,9 @@
+-- Analise de KILL dentro do banco de dados. Atraves do ERROR Log.
+
+-- últimas 6h procurando por eventos de KILL no Error Log
+DECLARE @desde DATETIME = DATEADD(HOUR,-24,GETDATE());
+
+EXEC xp_readerrorlog 0, 1, N'was killed by',  NULL, @desde, NULL, N'desc';  -- inglês
+EXEC xp_readerrorlog 0, 1, N'killed process', NULL, @desde, NULL, N'desc';  -- variação
+-- se seu SQL estiver em PT-BR e logar traduzido, tente:
+EXEC xp_readerrorlog 0, 1, N'foi morto',      NULL, @desde, NULL, N'desc';
